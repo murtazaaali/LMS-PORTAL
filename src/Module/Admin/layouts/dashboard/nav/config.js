45ace7aa8f9a48a4decad70495599ff64533879e
murtazaaali <alim02818@gmail.com>
2023-10-06 20:15:06 +0500

    updated Icon Pack

--- src/Module/Admin/layouts/dashboard/nav/config.js
+++ src/Module/Admin/layouts/dashboard/nav/config.js
@@ -1,42 +1,48 @@
 // component
-import SvgColor from '../../../components/svg-color';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import GroupIcon from '@mui/icons-material/Group';
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import AssessmentIcon from '@mui/icons-material/Assessment';
+import ContactPageIcon from '@mui/icons-material/ContactPage';
+// import SvgColor from '../../../components/svg-color';
 
 // ----------------------------------------------------------------------
 
-const icon = (name) => <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />;
+// const icon = (name) => <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />;
 
 const navConfig = [
   {
     title: 'dashboard',
     path: '/dashboard/app',
-    icon: icon('ic_analytics'),
+    icon: <DashboardIcon />,
   },
   {
     title: 'Create User',
     path: '/dashboard/createuser',
-    icon: icon('ic_user'),
+    icon: <PersonAddIcon />,
   },
   {
     title: 'Manage Team',
     path: '/dashboard/manageteam',
-    icon: icon('ic_user'),
+    icon: <GroupIcon />,
   },
 
   {
     title: 'Invocies',
     path: '/dashboard/invocies',
-    icon: icon('ic_user'),
+    icon: <ReceiptIcon />,
   },
 
   {
     title: 'Reports',
     path: '/dashboard/reports',
-    icon: icon('ic_user'),
+    icon: <AssessmentIcon />,
   },
   {
     title: 'Conatct',
     path: '/dashboard/conatct',
-    icon: icon('ic_user'),
+    icon: <ContactPageIcon />,
   },
 ];
 
